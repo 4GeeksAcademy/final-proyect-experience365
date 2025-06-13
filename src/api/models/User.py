b5cc82b906051d03e90f1b580c59c7f0c454f7b7
Melanie Dana Medigovich <melacem@gmail.com>
2025-06-13 17:07:59 +0000

    user-table

--- src/api/models/User.py
+++ src/api/models/User.py
@@ -1,15 +1,20 @@
-from flask_sqlalchemy import SQLAlchemy
+from api.database.db import db
 from sqlalchemy import String, Boolean
 from sqlalchemy.orm import Mapped, mapped_column
 
-db = SQLAlchemy()
 
 class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
+    __tablename__ = "User"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(nullable=True)
+    lastname: Mapped[str] = mapped_column(nullable=True)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(nullable=False)
+    is_active: Mapped[bool] = mapped_column(
+        Boolean(), nullable=False, default=True)
 
     def serialize(self):
         return {
