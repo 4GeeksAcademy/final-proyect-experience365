344c7ea1bde2f19bf6d00b190bb2d7f639c01432
BOTH-AGENCY <morellans@gmail.com>
2025-06-23 20:19:40 +0200

    Fixed message loading and token stored in local storage (#11)

--- src/api/models/Activity.py
+++ src/api/models/Activity.py
@@ -3,17 +3,16 @@ from sqlalchemy import String, Boolean, Text, ForeignKey, DateTime
 from sqlalchemy.orm import Mapped, mapped_column
 from datetime import datetime, timezone
 
+
 class Activity(db.Model):
 
     __tablename__ = 'activity'
     id: Mapped[int] = mapped_column(primary_key=True)
-    profesional_id: Mapped[int] = mapped_column(ForeignKey("Professional.id"), nullable=False)
+    profesional_id: Mapped[int] = mapped_column(
+        ForeignKey("Professional.id"), nullable=False)
     description: Mapped[str] = mapped_column(Text(), nullable=False)
     price: Mapped[int] = mapped_column(nullable=False)
     rate: Mapped[int] = mapped_column(nullable=False)
-    email: Mapped[str] = mapped_column(
-        String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
     # status: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
     is_active: Mapped[bool] = mapped_column(
@@ -30,9 +29,6 @@ class Activity(db.Model):
             "description": self.description,
             "price": self.price,
             "rate": self.rate,
-            "email": self.email,
-            "password": self.password,
-            # "status": self.status
             "is_active": self.is_active,
             "activity_date": self.activity_date.isoformat() if self.activity_date else None
         }
