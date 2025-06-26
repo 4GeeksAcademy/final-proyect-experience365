404cca664b024065d323e627754b0d581dac3ec4
Sergio García <sergio4geeks@gmail.com>
2025-06-26 17:54:25 +0000

    proffesional register

--- src/front/pages/RegisterProfessional.jsx
+++ src/front/pages/RegisterProfessional.jsx
@@ -76,7 +76,7 @@ export const RegisterProfessional = () => {
               <h2 className="card-title text-center mb-4">Regístrate como Profesional</h2>
               {error && <div className="alert alert-danger">{error}</div>}
               <form onSubmit={handleSubmit}>
-
+              {/* esto es el name */}
                 <div className="row">
                   <div className="col-md-6 mb-3">
                     <label htmlFor="name" className="form-label">Nombre</label>
