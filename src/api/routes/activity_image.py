17d82cc98683699c989d6c21eb3a5a1a76d69336
Melanie Dana Medigovich <melacem@gmail.com>
2025-07-08 21:50:09 +0200

    add images back fixed (#31)

--- src/api/routes/activity_image.py
+++ src/api/routes/activity_image.py
@@ -1,24 +1,34 @@
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
-from api.models import Activity, ActivityImage, Professional
+from api.models.Activity import Activity
+from api.models.ActivityImage import ActivityImage
+from api.models.Professional import Professional
 from api.database.db import db
+from flask_cors import CORS
 import cloudinary.uploader
 import os
 
 api = Blueprint('api/activity_images', __name__)
 
+CORS(api)
+
 # POST /api/activities/<activity_id>/images
+
+
 @api.route('/<int:activity_id>/images', methods=['POST'])
 @jwt_required()
 def upload_activity_images(activity_id):
     current_user_id = get_jwt_identity()
-    professional = Professional.query.filter_by(user_id=current_user_id).first()
-    
+    professional = Professional.query.filter_by(
+        user_id=int(current_user_id)).first()
+    print("professional", professional)
+
     # Verificar que el profesional es dueño de la actividad
     activity = Activity.query.filter_by(
-        id=activity_id, 
+        id=int(activity_id),
         profesional_id=professional.id
     ).first_or_404()
+    print("activity", activity)
 
     if 'files' not in request.files:
         return jsonify({"error": "No files provided"}), 400
@@ -29,10 +39,11 @@ def upload_activity_images(activity_id):
     for file in files:
         if file.filename == '':
             continue
-        
+
         # Subir a Cloudinary
         upload_result = cloudinary.uploader.upload(file)
         img_url = upload_result['url']
+        print("image_url", img_url)
 
         # Guardar en BD
         new_image = ActivityImage(
@@ -46,6 +57,8 @@ def upload_activity_images(activity_id):
     return jsonify({"images": uploaded_urls}), 201
 
 # GET /api/activities/<activity_id>/images
+
+
 @api.route('/<int:activity_id>/images', methods=['GET'])
 def get_activity_images(activity_id):
     activity = Activity.query.get_or_404(activity_id)
