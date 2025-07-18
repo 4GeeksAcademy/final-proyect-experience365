9dbda8e3313b12d76f5ad9997c72bc5c8dd5b011
BOTH-AGENCY <morellans@gmail.com>
2025-07-18 19:03:43 +0000

    changes

--- migrations/versions/2ce03beb7f16_.py
+++ migrations/versions/2ce03beb7f16_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 93df87fc10db
+Revision ID: 2ce03beb7f16
 Revises: 
-Create Date: 2025-07-17 22:50:16.696857
+Create Date: 2025-07-18 16:12:44.491351
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '93df87fc10db'
+revision = '2ce03beb7f16'
 down_revision = None
 branch_labels = None
 depends_on = None
